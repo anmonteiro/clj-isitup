882033d77db4ec3e146ec346d0ea3badb507bcc7
António Nuno Monteiro <anmonteiro@gmail.com>
2015-10-22 21:16:22 +0200

    deps, remove sublime-project

--- project.clj
+++ project.clj
@@ -1,12 +1,14 @@
 (defproject isitup "0.3.0"
   :description "isitup.org API command line consumer"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/anmonteiro/isitup-cli"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.cli "0.3.1"]
-                 [clj-http "1.0.0"]
-                 [org.clojure/data.json "0.2.5"]]
+                 ;; need to use a SNAPSHOT version because of
+                 ;; https://github.com/dakrone/clj-http/issues/275
+                 [clj-http "3.0.0-SNAPSHOT"]
+                 [org.clojure/data.json "0.2.6"]]
   :main ^:skip-aot isitup.core
   :target-path "target/%s"
   :clean-targets [:target-path]
