9f87a61e5612f373d593f20fda072b77b8417efc
António Nuno Monteiro <anmonteiro@gmail.com>
2015-11-14 13:43:14 +0100

    next iteration

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-isitup "0.5.0"
+(defproject clj-isitup "0.6.0-SNAPSHOT"
   :description "isitup.org API client and command line tool"
   :url "https://github.com/anmonteiro/clj-isitup"
   :license {:name "Eclipse Public License"
@@ -7,7 +7,7 @@
                  [org.clojure/tools.cli "0.3.3"]
                  ;; need to use a SNAPSHOT version because of
                  ;; https://github.com/dakrone/clj-http/issues/275
-                 [clj-http "3.0.0-20150829.034655-1"]
+                 [clj-http "3.0.0-SNAPSHOT"]
                  [clojurewerkz/urly "1.0.0"]
                  [org.clojure/data.json "0.2.6"]]
   :main ^:skip-aot clj-isitup.cli
