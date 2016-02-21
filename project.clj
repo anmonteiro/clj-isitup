f35876115ff3d6344872bf33a2c6d1ea8e21d0d6
António Nuno Monteiro <anmonteiro@gmail.com>
2016-02-21 23:04:01 +0100

    clojure 1.8

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/anmonteiro/clj-isitup"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0-RC5" :scope "provided"]
+  :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
                  [org.clojure/tools.cli "0.3.3"]
                  ;; need to use a SNAPSHOT version because of
                  ;; https://github.com/dakrone/clj-http/issues/275
